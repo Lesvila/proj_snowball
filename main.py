cc8d54f8d901bb452776fd4c92f2fbf0c080e2c8
Lesvila <cloud.lee12@gmail.com>
2023-02-25 16:23:02 +0800

    v2

--- main.py
+++ main.py
@@ -7,22 +7,25 @@ import numpy as np
 from math import exp, sqrt, log
 import matplotlib.pyplot as plt
 from pandas_market_calendars import get_calendar
+from collections import Counter
 
 
 class SnowBall:
 
-    def __init__(self, s0, s_val, rf, bp, vol, 
+    def __init__(self, s0, s_val, rf, bp, vol, discount_rate, 
                 start, end, val_date,
                 knock_in,
                 knock_out, knock_out_step,
                 ob: list,
                 out, in_range, in_not_out,
+                deposit, knocked_in,
                 trigger): #out:敲出  in_range: 未敲入未敲出  in_not_out: 敲入但未敲出
         self.s0 = s0
         self.s_val = s_val
         self.rf = rf
         self.bp = bp
         self.vol = vol
+        self.discount_rate = discount_rate
         self.start = start
         self.end = end
         self.val_date = val_date
@@ -33,6 +36,8 @@ class SnowBall:
         self.out = out
         self.in_range = in_range
         self.in_not_out = in_not_out
+        self.deposit = deposit
+        self.knocked_in = knocked_in
         self.trigger = trigger
 
 
@@ -49,6 +54,18 @@ class SnowBall:
     def get_ob_days(self):
         ob_days = [str(datetime.strptime(date, "%Y-%m-%d")) for date in self.ob]
         return ob_days
+    
+    def get_flags(flag_result : list):
+        if flag_result == [0, 1]:
+            result = 1 #not in but out
+        elif flag_result == [1, 1]:
+            result = 2 # in and out
+        elif flag_result == [1, 0]:
+            result = 3 # in not out
+        else:
+            result = 4 # not in not out
+        
+        return result
 
 
     def simulation(self, bd_list, bd_lens, ob_days):
@@ -56,63 +73,69 @@ class SnowBall:
         s = np.zeros(bd_lens+1)
         t = [0]
         s[0] = self.s_val
+        start = datetime.strptime(self.start, "%Y-%m-%d")
+        val_date = datetime.strptime(self.val_date, "%Y-%m-%d")
+        htm_delta = datetime.strptime(self.end, "%Y-%m-%d") - start
+        htm_delta_discount = datetime.strptime(self.end, "%Y-%m-%d") - val_date
+        flag_in = self.knocked_in
+        flag_out = 0
 
         for i in range(0, bd_lens):
             s[i+1] = s[i]*exp((self.rf - self.bp - 0.5 * self.vol ** 2) * 1/252 + self.vol * sqrt(1/252) * np.random.randn())
 
-        for bd_day in bd_list:
-            bd_index = bd_list.index(bd_day)
-
-            flag_in = 0
-            flag_out = 0
-            price = s[bd_index]
-            
-            start = datetime.strptime(self.start, "%Y-%m-%d")
-            val_date = datetime.strptime(self.val_date, "%Y-%m-%d")
-
-            htm_delta = datetime.strptime(self.end, "%Y-%m-%d") - start
-            htm_delta_discount = datetime.strptime(self.end, "%Y-%m-%d") - val_date
-
+        for bd_day in bd_list[1:]:
+            price = s[bd_list.index(bd_day)]
             if bd_day in ob_days:
                 ob_day_index = ob_days.index(bd_day)  #判断第几个观察日
-                knock_out = self.s0 * (self.knock_out + self.knock_out_step * ob_day_index)
+                knock_out = self.s0 * (self.knock_out - self.knock_out_step * ob_day_index)
                 if price > knock_out:
                     delta = datetime.strptime(bd_day, "%Y-%m-%d %H:%M:%S") - start
                     discount = datetime.strptime(bd_day, "%Y-%m-%d %H:%M:%S") - val_date
+                    flag_out = 1
+                    flag_in = 1 if ((np.min(s[1:bd_list.index(bd_day)]) < self.knock_in * self.s0) + self.knocked_in) > 0 else 0
                     if self.trigger:
-                        payoff = 1+ (self.out * (delta.days + 1) / 365) *  exp(-self.rf * (discount.days + 1)/ 365) #收益率
+                        payoff = 1 + (self.out * (delta.days + 1) / 365) *  exp(-self.discount_rate * (discount.days + 1)/ 365) #收益率
                     else:
-                        payoff = (1 + self.out * (delta.days + 1) / 365) *  exp(-self.rf * (discount.days + 1)/ 365) #收益率
-                    break
-            
-            elif s[bd_index] < self.knock_in * self.s0:
-                if self.trigger:
-                    payoff = 1+ (eval(self.in_not_out)) *  exp(-self.rf * (htm_delta_discount.days + 1)/ 365)
-                else:
-                    payoff = (1 + eval(self.in_not_out)) *  exp(-self.rf * (htm_delta_discount.days + 1)/ 365)
+                        payoff = 1 + self.deposit * (exp(-self.discount_rate * (discount.days + 1)/ 365) - 1) + self.out * (delta.days + 1) / 365 *  exp(-self.discount_rate * (discount.days + 1)/ 365) #收益率
                 break
 
-            else:
-                if self.trigger:
-                    payoff = 1+ (self.in_range * (htm_delta.days+1) / 365) * exp(-self.rf * (htm_delta_discount.days + 1)/ 365)
+            if flag_out == 0:
+                if np.min(s[1:]) < self.knock_in * self.s0 or flag_in == 1:
+                    flag_in = 1
+                    if self.trigger:
+                        payoff = 1 + (eval(self.in_not_out)) *  exp(-self.discount_rate * (htm_delta_discount.days + 1)/ 365)
+                    else:
+                        payoff = 1 + self.deposit * (exp(-self.discount_rate * (htm_delta_discount.days + 1)/ 365) - 1) + eval(self.in_not_out) *  exp(-self.discount_rate * (htm_delta_discount.days + 1)/ 365)
+                    break
+
                 else:
-                    payoff = (1 + self.in_range * (htm_delta.days+1) / 365) * exp(-self.rf * (htm_delta_discount.days + 1)/ 365)
+                    flag_in == 0
+                    if self.trigger:
+                        payoff = 1+ (self.in_range * (htm_delta.days+1) / 365) * exp(-self.discount_rate * (htm_delta_discount.days + 1)/ 365)
+                    else:
+                        payoff = 1 + self.deposit * (exp(-self.discount_rate * (htm_delta_discount.days + 1)/ 365) -1 ) + self.in_range * (htm_delta.days+1) / 365 * exp(-self.discount_rate * (htm_delta_discount.days + 1)/ 365)
 
-        return payoff
+        return payoff, [flag_in, flag_out]
 
 
-test1 = SnowBall(6819.43, 5864.47, 0.0214, 0.048, 0.2074, '2022-2-25', '2024-2-26', '2022-12-31', 0.75, 0.96, -0.005, ['2023-1-30','2023-2-27', '2023-3-27','2023-4-25','2023-5-25','2023-6-26','2023-7-25','2023-8-25','2023-9-25','2023-10-25','2023-11-27','2023-12-25','2024-1-25','2024-2-26',], 0.152, 0.152, 'min(0,s[-1]/self.s0 - 1)', True)
-bd_list = SnowBall.get_bd(test1)[0]
-bd_lens = SnowBall.get_bd(test1)[1]   
-ob_days = SnowBall.get_ob_days(test1)  
-result = []        
-for i in range(0,1000):
-    result.append(SnowBall.simulation(test1, bd_list, bd_lens, ob_days))
-    # print(result)
+test1 = SnowBall(7121.51, 5864.47, 0.01, 0.165, 0.16, 0.02, '2021-11-15', '2023-11-13', '2022-12-31', 0.75, 1.00, 0, ['2023-1-12','2023-2-13', '2023-3-13','2023-4-12','2023-5-12','2023-6-12','2023-7-12','2023-8-14','2023-9-12','2023-10-12','2023-11-13',], 0.12, 0.12, 'max(-0.25, min(0, s[-1]/self.s0 - 1))', 0.25, 0, True)
+# test1 = SnowBall(1, 1, 0.03, 0, 0.13, '2022-1-1', '2022-12-31', '2022-1-1', 0.85, 1.03, 0, ['2022-1-31', '2022-2-28', '2022-3-31', '2022-4-30', '2022-5-31', '2022-6-30', '2022-7-31', '2022-8-31', '2022-9-30', '2022-10-31', '2022-11-30', '2022-12-31'], 0.2, 0.2, 'min(0, s[-1]/self.s0 - 1)', False)
+# bd_list = SnowBall.get_bd(test1)[0]
+# bd_lens = SnowBall.get_bd(test1)[1]   
+# ob_days = SnowBall.get_ob_days(test1)
+# result = []
+# flag_results = []     
+# for i in range(0,10000):
+#     result.append(SnowBall.simulation(test1, bd_list, bd_lens, ob_days)[0])
+#     flag_results.append(SnowBall.get_flags(SnowBall.simulation(test1, bd_list, bd_lens, ob_days)[1]))
 
-if __name__ == '__main__':
-    print(f'average result: {np.mean(result)}')
-    n, bins, patches = plt.hist(pd.DataFrame(result), 50, density=True, alpha=0.75)
-    plt.vlines(x = np.mean(result), ymin=0, ymax=max(n), colors='r')
-    plt.text(x=np.mean(result), y=max(n), s='{0:.2f}'.format(np.mean(result)))
-    plt.show()
+#     print(np.mean(result))
+#     print(Counter(flag_results))
+
+
+# if __name__ == '__main__':
+#     print(f'average result: {np.mean(result)}')
+#     n, bins, patches = plt.hist(pd.DataFrame(result), 100, density=True, alpha=0.75)
+#     plt.vlines(x = np.mean(result), ymin=0, ymax=max(n), colors='r')
+#     plt.text(x=np.mean(result), y=max(n), s='{0:.2f}'.format(np.mean(result)))
+#     plt.show()
--- snowball.py
+++ snowball.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 from main import *
+from collections import Counter
 import plotly.express as px
 import matplotlib.pyplot as plt
 
@@ -37,12 +38,14 @@ file = st.sidebar.file_uploader(' ', type='xlsx', key=None)
 with st.form(key='parameters input'):
     with st.sidebar:
         rf = st.number_input('无风险利率', step = 0.1, format='%.2f')
+        discount_rate = st.number_input('折现率', step = 0.1, format='%.2f')
         bp = st.number_input('基差', step = 0.1, format='%.2f')
         vol = st.number_input('年化波动率', step = 0.1, format='%.2f')
         trials = st.number_input('模拟次数', step=1000)
         trigger_check = st.checkbox('本金不折现')
+        knocked_in = st.checkbox('已经敲入')
 
-        submit_button = st.form_submit_button(label='开始模拟')
+        submit_button = st.form_submit_button(label='开始模拟', )
 
 st.sidebar.title("About")
 st.sidebar.info(
@@ -108,11 +111,13 @@ try:
         return_range = data_main.loc[14]['数值']
         return_innoout = data_main.loc[15]['数值']
         participate = data_main.loc[16]['数值']
+        deposit = data_main.loc[17]['数值']
 
         st.markdown('年化敲出收益率: <span style="color:green;"> {:.2%} </span>'.format(return_out),unsafe_allow_html=True)
         st.markdown('到期收益率: <span style="color:green;"> {:.2%} </span>'.format(return_range),unsafe_allow_html=True)
         st.markdown('敲入未敲出收益率: <span style="color:green;"> {} </span>'.format(return_innoout),unsafe_allow_html=True)
         st.markdown('参与率: <span style="color:green;"> {:.2%} </span>'.format(participate),unsafe_allow_html=True)
+        st.markdown('预付金率: <span style="color:green;"> {:.2%} </span>'.format(deposit),unsafe_allow_html=True)
 
     with obdays_info:
         st.header('剩余观察日')
@@ -124,55 +129,71 @@ except:
 
 
 if submit_button:
+    for i in ['result', 'flag_result']:
+        if i not in st.session_state:
+            st.session_state[i] = ""
 
-    sample = SnowBall(s0=s0, s_val=s_val, rf = rf/100, bp = bp/100, vol = vol/100, 
+    sample = SnowBall(s0=s0, s_val=s_val, rf = rf/100, bp = bp/100, vol = vol/100, discount_rate = discount_rate/100,
     start=start_date, end=end_date, val_date=val_date, 
     knock_in=knock_in,
     knock_out=knock_out, knock_out_step=knock_out_step,
     ob = ob_list,
     out = return_out, in_range = return_range, in_not_out= return_innoout,
+    deposit=deposit,
+    knocked_in=knocked_in,
     trigger= trigger_check)
 
     bd_list = SnowBall.get_bd(sample)[0]
     bd_lens = SnowBall.get_bd(sample)[1]   
-    print(bd_list)
     ob_days = SnowBall.get_ob_days(sample)
 
-    def generate_result(trials, sample, bd_list, bd_lens, ob_days):    
-        result = [] 
-        for i in range(0, trials):
-            result.append(SnowBall.simulation(sample, bd_list, bd_lens, ob_days))
-        return result
+    # def generate_result(trials, sample, bd_list, bd_lens, ob_days):    
+    result = []
+    flag_result = [] 
+    for _ in range(0, trials):
+        result.append(SnowBall.simulation(sample, bd_list, bd_lens, ob_days)[0])
+        flag_result.append(SnowBall.get_flags(SnowBall.simulation(sample, bd_list, bd_lens, ob_days)[1]))
+        # return result, flag_result
 
-    st.session_state.result = generate_result(trials, sample, bd_list, bd_lens, ob_days)
-
-
-result_plot, result_stats = st.columns(2)
-
-with result_plot:
-    try:
-        if 'result' not in st.session_state:
-            st.session_state['result'] = []
-        print(f'average result: {np.mean(st.session_state.result)}')
-        
-        n, bins, patches = plt.hist(st.session_state.result, bins=100)
-
-        fig = px.histogram(data_frame = np.array(st.session_state.result), nbins=100, opacity=0.75, 
-        title='{} - {} \n 整体估值: {:,}'.format(contract, target_ticker, round( participate * principal * np.mean(st.session_state.result))), labels=None,
-        range_y=(0, n))
-        mean = np.mean(st.session_state.result)
-        median = np.median(st.session_state.result)
-
-        fig.add_annotation(dict(font=dict(color='red',size=14),
-                                            x=mean,
-                                            y=max(n),
-                                            showarrow=False,
-                                            text="单位平均数: {:.4f}".format(mean),
-                                            textangle=0,
-                                            xanchor='right'))
-        fig.add_vline(x=np.mean(st.session_state.result))
+    st.session_state.result = result
+    st.session_state.flag_result =flag_result
 
 
-        st.plotly_chart(fig)
-    except:
-        pass
+    result_plot, result_stats = st.columns(2)
+
+
+    with result_plot:
+        try:
+            print(f'average result: {np.mean(st.session_state.result)}')
+            
+            n, bins, patches = plt.hist(st.session_state.result, bins=100)
+
+            fig = px.histogram(data_frame = np.array(st.session_state.result), nbins=100, opacity=0.75, 
+            title='{} - {} \n 整体估值: {:,}'.format(contract, target_ticker, round( participate * principal * np.mean(st.session_state.result))), labels=None,
+            range_y=(0, n))
+            mean = np.mean(st.session_state.result)
+            median = np.median(st.session_state.result)
+
+            fig.add_annotation(dict(font=dict(color='red',size=14),
+                                                x=mean,
+                                                y=max(n),
+                                                showarrow=False,
+                                                text="单位平均数: {:.4f}".format(mean),
+                                                textangle=0,
+                                                xanchor='right'))
+            fig.add_vline(x=np.mean(st.session_state.result))
+
+            st.plotly_chart(fig)
+        except:
+            pass
+
+    with result_stats:
+        try:
+            pattern = Counter(st.session_state.flag_result)
+            print(pattern)
+            st.write('未敲入直接敲出次数: {:,}, 概率: {:.2%}'.format(pattern[1], pattern[1]/trials))
+            st.write('敲入后直接敲出次数: {:,}, 概率: {:.2%}'.format(pattern[2], pattern[2]/trials))
+            st.write('敲入后但未敲出次数: {:,}, 概率: {:.2%}'.format(pattern[3], pattern[3]/trials))
+            st.write('未敲入且未敲出次数: {:,}, 概率: {:.2%}'.format(pattern[4], pattern[4]/trials))
+        except:
+            pass
